98de868fb003961e7d5773a5c408417e3ea0ee1e
code-intenssive <justtega97@gmail.com>
2022-08-21 11:01:37 +0100

    image path validation and get images from dir

--- py_resizer/py_image_resizer.py
+++ py_resizer/py_image_resizer.py
@@ -17,65 +17,76 @@ def get_supported_suffixes_as_str() -> str:
 
 def validate_image_path(img_path: Union[Path, str]) -> bool:
     """
-    Carries out basic validation on the image path
+    Carries out validation on the image path
     """
+    img_path = Path(img_path)
     # Validate the image path, type and extension
     if not img_path.exists():
         raise FileNotFoundError(
-            '%s was not found in your path, kindly review the path provided' % img_path)
-    elif not img_path.is_file():
+            F'{ img_path } was not found in your path, kindly review the path provided')
+
+    if not img_path.is_file():
         raise NotAFileError(
-            '%s exists but is not a file, kindly pass a valid file path' % img_path)
-    elif not img_path.suffix.lower()[1:] in (SUPPORTED_SUFFIXES):
+            F'{ img_path } exists but is not a file, kindly pass a valid file path')
+
+    if not img_path.suffix.lower()[1:] in (SUPPORTED_SUFFIXES):
         raise InvalidImageTypeError(
             'The image file extension is not supported, '
-            'supported extensions must be one of the follwing %s' % get_supported_suffixes_as_str())
-    return True
+            F'supported extensions must be one of the following { get_supported_suffixes_as_str() }')
+
+    return img_path
 
 
 def validate_dir_path(img_path: Union[Path, str]) -> bool:
     """
-    Carries out basic validation on the directory provided
+    Carries out validation on the directory provided
     """
+    img_path = Path(img_path)
     # Validate the image path, type and extension
     if not img_path.exists():
         raise FileNotFoundError(
-            '%s was not found in your path, kindly review the path provided' % img_path)
-    elif not img_path.is_dir():
+            F'{ img_path } was not found in your path, kindly review the path provided')
+
+    if not img_path.is_dir():
         raise NotADirectoryError(
-            '%s exists but is not a directory, kindly pass a valid directory path' % img_path)
+            F'{ img_path } exists but is not a directory, kindly pass a valid directory path')
+
+    return img_path
 
 
 def get_img_paths_from_img_dir(img_dir: Union[Path, str], *, extensions: Tuple[str] = SUPPORTED_SUFFIXES,
-                               recursive:bool=False, exit_if_empty: bool = True) -> List[Path]:
+                               recursive: bool = False, exit_if_empty: bool = True) -> List[Path]:
     """
     Returns a list of image file paths matching the
     specified extension within the given directory
     """
-    if isinstance(img_dir, str):
-        img_dir = Path(img_dir)
+    img_dir = validate_dir_path(img_dir)
     found_images = []
     search_dir = img_dir.rglob if recursive else img_dir.glob
     start_message = 'Recursively searching for image files at %s' if recursive else 'Searching for image files at %s'
     print(start_message % img_dir)
+
     for extension in extensions:
-        img_pattern = '*.%s' % extension
+        img_pattern = F'*.{ extension }'
         extension = extension.upper()
         found_image = list(search_dir(img_pattern))
         # If we are unable to find any of image of the
         # extension type, we notify the user and continue
         # the loop from the top
         if not found_image:
-            print('No %s image found' % extension)
+            print(F'No { extension } image found')
             continue
+
         found_images.extend(found_image)
-        print('Found %d %s image(s)' % (len(found_image), extension))
+        print(F'Found { len(found_image) } { extension } image(s)')
+
     if not found_images:
-        print('No image file was found' % (img_dir))
+        print(F'No image file was found in { img_dir }')
         if exit_if_empty:
             exit(0)
     else:
-        print('Found %d image(s) at %s' % (len(found_images), img_dir))
+        print(F'Found { len(found_images) } image(s) at { img_dir }')
+
     return found_images
 
 
@@ -99,9 +110,8 @@ def resize_image(img_path: Union[Path, str], *, extension: str = None, img_heigh
 
     * `returns` : `bool` -> `True` if the image was resized else `False`
     """
-    if isinstance(img_path, str):
-        img_path = Path(img_path)
-    img_path = img_path.resolve()
+    img_path = Path(img_path).resolve()
+
     # perform basic validation
     error_message = None
     try:
@@ -116,30 +126,31 @@ def resize_image(img_path: Union[Path, str], *, extension: str = None, img_heigh
         image = Image.open(img_path)
         # if a new extension is required as output format
         # set the current file's extension type to it
-        resized_image = image.resize((img_height, img_width), Image.ANTIALIAS)
+        resized_image = image.resize((img_width, img_height), Image.ANTIALIAS)
+
         if extension:
             extension = extension.lower() if extension.startswith(
                 '.') else '.' + extension.lower()
             img_path = img_path.with_suffix(extension)
         # add a new prefix image file before saving
-        resized_image_name = 'resized_%s' % img_path.name
+        resized_image_name = F'resized_{ img_path.name }'
         img_format = img_path.suffix.upper()[1:]
         img_path = img_path.with_name(resized_image_name)
         resized_image.save(img_path, img_format, quality=90)
-        print('Resized image was saved at %s in %s format' %
-              (img_path, img_format))
+        print(
+            F'Resized image to ({ img_width }x{ img_height }) was saved at { img_path } in { img_format } format')
         return True
     print(F'Image resizing failed for {img_path}...\n{error_message},\n')
     return False
 
 
-def resize_bulk_images(*, img_paths: Union[Tuple[Path], Tuple[str]] = (), img_dir: Union[Path, str ]= None,
-                       extensions: str = None, img_height: int = 200, img_width: int = 200) -> None:
+def resize_bulk_images(*, img_paths: Union[Tuple[Path], Tuple[str]] = (), img_dir: Union[Path, str] = None, save_as='png',
+                       extensions: str = None, img_height: int = 200, img_width: int = 200, recursive: bool = False) -> None:
     """
     Resizes an image file to the new height and width as well as
     a new extension type if extension is provided
 
-    `img_paths`:`Tuple[Path]` | `Tuple[str]` -> Tuple containing paths of images to be processed
+    `img_paths`:`Tuple[Path]` | `Tuple[str]` -> Tuple containing paths or string representation of the image paths to be processed
 
     `img_dir`:`Path | str` -> Folder containing images to be processed
 
@@ -164,11 +175,11 @@ def resize_bulk_images(*, img_paths: Union[Tuple[Path], Tuple[str]] = (), img_di
 
     # we use the img_paths argument if it is provided,
     # use the img_dir if the img_paths is not
-    image_paths = img_paths if img_paths else get_img_paths_from_img_dir(
-        img_dir, extensions=extensions or SUPPORTED_SUFFIXES)
+    image_paths = img_paths or get_img_paths_from_img_dir(
+        img_dir, extensions=extensions or SUPPORTED_SUFFIXES, recursive=recursive)
 
     kwargs = {
-        'extension': extensions,
+        'extension': save_as,
         'img_height': img_height,
         'img_width': img_width
     }
